3ef292566122b1359505303e39feb5a2282022c9
Yuvraj8090 <yuvrajkohli91058@gmail.com>
2026-01-16 16:28:45 +0530

    s

---
+++ common/head.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
+
+<link rel="stylesheet" href="css/style.css">
+
+<link rel="stylesheet" href="css/menu.css">
+
+<link rel="stylesheet" href="css/responsive.css">
