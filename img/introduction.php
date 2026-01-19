129269cc6234d456945d776bb4b4d1647ed9388e
Yuvraj8090 <135937985+Yuvraj8090@users.noreply.github.com>
2026-01-19 13:26:42 +0530

    Update introduction.php

--- img/introduction.php
+++ img/introduction.php
@@ -1,217 +1,239 @@
-<p>
-  Forest Survey of India assess the forest cover mapping of the country on a
-  two-year cycle using satellite data. The main objective is presentation of the
-  information on forest resources of the country at state and district level and
-  to prepare forest cover maps on 1:50,000 scale. First assessment of forest
-  cover of the country was made in 1987 and thereafter eight more assessments
-  have been made. District wise information on forest cover has been made
-  available from the third assessment (i.e. from 1991) onwards.
-</p>
-
-<p>
-  Till the fourth assessment interpretation of data for the entire country had
-  been done visually. During fifth and sixth assessment interpretation of data
-  for Madhya Pradesh and Maharashtra which comprises 28% forest cover of the
-  country had been done digitally and for rest of the States/UTs, it had been
-  done visually. In seventh assessment interpretation has been done digitally
-  for thirteen states namely; Andhra Pradesh, Arunachal Pradesh, Assam, Delhi
-  (UT), Himachal Pradesh, Madhya Pradesh, Maharashtra, Manipur, Meghalaya,
-  Mizoram, Nagaland, Sikkim and Tripura, which comprises 63% of the forest cover
-  of the country. By eighth assessment the entire country was covered by digital
-  assessment.
-</p>
-
 <div class="table-responsive">
   <table class="table table-bordered">
     <thead>
       <tr class="active">
-        <th scope="col"><span class="heading1">Cycle of Assessment</span></th>
-        <th scope="col" class="heading1">Year</th>
-        <th scope="col" class="heading1">Data Period</th>
-        <th scope="col" class="heading1">Sensor</th>
-        <th scope="col" class="heading1">Spatial Resolution</th>
-        <th scope="col" class="heading1">scale</th>
-        <th scope="col" class="heading1">Minimum Mappable Unit (ha)</th>
-        <th scope="col" class="heading1">Mode of Interpretation</th>
+        <th scope="col">Project / Report Title</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row" class="heading2">First</th>
-        <td>1987</td>
-        <td>1981-1983</td>
-        <td>LANDSAT-MSS</td>
-        <td>80 m</td>
-        <td>1:1 million</td>
-        <td>400</td>
-        <td>Visual</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Second</th>
-        <td>1989</td>
-        <td>1985-87</td>
-        <td>LANDSAT-TM</td>
-        <td>30 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>Visual</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Third</th>
-        <td>1991</td>
-        <td>1987-89</td>
-        <td>LANDSAT-TM</td>
-        <td>30 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>Visual</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Fourth</th>
-        <td>1993</td>
-        <td>1989-91</td>
-        <td>LANDSAT-TM</td>
-        <td>30 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>visual</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Fifth</th>
-        <td>1995</td>
-        <td>1991-93</td>
-        <td>IRS-1B LISSII</td>
-        <td>36.25 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>Visual &amp; Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Sixth</th>
-        <td>1997</td>
-        <td>1993-95</td>
-        <td>IRS-1B LISSII</td>
-        <td>36.25 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>Visual &amp; Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Seventh</th>
-        <td>1999</td>
-        <td>1996-98</td>
-        <td>IRS-1C/1D<br />LISS III</td>
-        <td>23.5 m</td>
-        <td>1:250,000</td>
-        <td>25</td>
-        <td>Visual &amp; Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Eighth</th>
-        <td>2001</td>
-        <td>2000</td>
-        <td>IRS-1C/1D<br />LISS III</td>
-        <td>23.5</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Ninth</th>
-        <td>2003</td>
-        <td>2002</td>
-        <td>IRS-1D LISS III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Tenth</th>
-        <td>2005</td>
-        <td>2004</td>
-        <td>IRS-1D LISS III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Eleventh</th>
-        <td>2009</td>
-        <td>2006</td>
-        <td>IRS-P6-LISS III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">twelth</th>
-        <td>2011</td>
-        <td>2008-2009</td>
-        <td>IRS-P6-LISS III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
-      </tr>
-      <tr>
-        <th scope="row" class="heading2">Thirteen</th>
-        <td>2013</td>
-        <td>2010-11</td>
+      <tr class="active">
         <td>
-          IRS-P6-LISS III<br />IRS-Resourcesat-2<br />
-          LISS III
+          <a href="vegetation-map-of-ganges-basin">
+            <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+            Vegetation Map of the Ganga Basin
+          </a>
         </td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
       </tr>
       <tr>
-        <th scope="row" class="heading2">Fourteen</th>
-        <td>2015</td>
-        <td>2013-14</td>
         <td>
-          IRS P6 LISS III<br />IRS-Resourcesat-2<br />LISS-III
+          <a href="http://fsi.nic.in/preparation-of-working-plan-of-ramnagar(UP)-using-remote-sensing-and-GIS">
+            <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+            Preparation of Working Plan of Ramnagar (UP) using Remote Sensing and GIS (1996-97)
+          </a>
         </td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Forest fire detection &amp; monitoring.
+        </td>
       </tr>
       <tr>
-        <th scope="row" class="heading2">Fifteen</th>
-        <td>2017</td>
-        <td>2015-16</td>
-        <td>IRS-Resourcesat-2<br />LISS-III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
+        <td>
+          <a href="status-of-forest-cover-in-biosphere-reserve">
+            <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+            Status of Forest Cover in Biosphere Reserve.
+          </a>
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          National spatial data infrastructure (NSDI).
+        </td>
       </tr>
       <tr>
-        <th scope="row" class="heading2">Sixteen</th>
-        <td>2019</td>
-        <td>2017-18</td>
-        <td>IRS-Resourcesat-2 <br />LISS-III</td>
-        <td>23.5 m</td>
-        <td>1:50,000</td>
-        <td>1</td>
-        <td>Digital</td>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          NATCOM (Indias initial National Communication to United Nations Framework convention on climate change).
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Pasture Lands in Himalayan Alpine Pastures in the Country 1989.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Application of GIS in Watershed Management -1991.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Status of Forest Cover in Tiger Reserves - 1992 - 93.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Burnt Areas due to Forest Fire - 1995.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Damage Assessment due to Super Cyclone in Orissa - 1999.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Burnt Area in UP Hills (Uttaranchal) - 1999.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Forest Cover Change Assessment using Remote Sensing Technology for FRA 2000 - 2000.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Valuation and Evaluation of Trees Outside Forests (TOF) for South East Asia for FAO - FRA 2000 - 1999.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Growing Stock, Biomass and Carbon in India’s Forests - 2004.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Forest Cover in Tiger Reserves of India: Status and Changes - 2006
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Rapid Assessment of Damage to Ecological Resources due to Tsunami - 2005.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Pilot Study on Assessment of Status of Sustainability of Forest Resources in India - 2003
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Report on Inventory of Trees Outside Forests (TOF) in Punjab - 2006
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventory of Trees Outside Forests in Uttar Pradesh - 2007
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventory of Trees Outside Forests in Uttarakhand - 2008
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventory of Trees Outside Forests in Karnataka - 2008
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Forest Cover in Adjoining Areas of the proposed Link Canal and Water Reservoirs - 2003.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Coral Reefs in India - 2003.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Assessment of Mangroves in India - 2003
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Near Real Time Monitoring of Active Forest Fires using MODIS Web Fire Mapper - 2004.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Coastal Zone Studies: Mapping at Community Level and Health Assessment of Mangrove Ecosystem - 2006
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          National Forest Type Mapping - 2006
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Verifying Land Cover in Himachal for Carbon Credit under CDM - 2008
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Mapping of Village - wise land use change in the Aravali Region of Haryana - 2008
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventory of GHG emissions for NATCOM - II - 2008
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Monitoring of forest cover change in tiger reserve in india - 2009
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Macro level EIA Studies of Bellary, Chitradurga and Tumkur districts of Karnataka - 2011
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Status, Density and Change in Forest Cover of Tiger Reserves in respect of Shivalik Gangetic Plain Landscape - 2013 - 2015.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventorization and Monitoring of Biosphere Reserves India under NNRMS Scheme - 2011 - 15.
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Forest Cover Mapping and Inventory of Forests /Tree Resources in Nagaland - 2012.
+        </td>
+      </tr>
+      <tr class="active">
+        <td>
+          <i aria-hidden="true" class="fa fa-caret-right" style="margin-right: 8px;"></i>
+          Inventory of Trees Outside Forests in five districts of Uttarakhand - 2015.
+        </td>
       </tr>
     </tbody>
   </table>
 </div>
-
-<p>
-  Forest Survey of India supplies hard &amp; soft copies of forest cover maps of
-  the country, states and districts as per the following rates:
-</p>
-
-<ul>
-  <li>
-    <p>A0 size Soft copy (TIFF/IMG format) is available for Rs.2000 in CDs.</p>
-  </li>
-</ul>
